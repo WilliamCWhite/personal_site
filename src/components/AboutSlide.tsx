2c1597ffa864665151ecbfd0d771bec3b09c9ac7
WilliamCWhite <williamcwhite323@gmail.com>
2025-11-29 22:11:21 -0500

    Added links to about slide

--- src/components/AboutSlide.tsx
+++ src/components/AboutSlide.tsx
@@ -1,15 +1,34 @@
 import Skill from "./Skill";
 
+import linkedinIcon from "../assets/icons/linkedin.png"
+import githubIcon from "../assets/icons/icons8-github-144.png"
+
 function AboutSlide() {
   return (
     <div className="slide-shadow rounded-xl p-3 flex flex-col justify-center md:flex-row md:items-stretch md:px-6 md:py-12">
       <div className="md:w-1 md:grow">
         <p className="">
-          Hi, I'm <span className="yellow">William</span>, a <span className="cyan">Software Engineer</span> studying <span className="blue">Computer Science</span> and <span className="purple">Philosophy</span> at <span className="red">Colgate University</span>.
+          Hi, I'm <span className="yellow font-bold">William</span>, a <span className="cyan">Software Engineer</span> studying <span className="blue">Computer Science</span> and <span className="purple">Philosophy</span> at <span className="red">Colgate University</span>.
           I love <span className="green">problem solving</span> and <span className="cyan">getting lost</span> in the <span className="yellow">experience</span> of <span className="blue">making things</span>.
-          This website and some of my <span className="purple">projects</span> are hosted on an <span className="red">EC2 instance</span> running an <span className="green">NGINX web server</span>, a <span className="blue">PostgreSQL database</span>, and <span className="blue">Docker containers</span> for the <span className="cyan">backend</span>.
+          This website and some of my <span className="purple">projects</span> are hosted on an <span className="red">EC2 instance</span> running an <span className="green">NGINX web server</span>, a <span className="blue">PostgreSQL database</span>, and <span className="blue">Docker containers</span> for the <span className="cyan">backend servers</span>.
           It shows off <span className="yellow">the projects</span> I've <span className="purple">(finished)</span> working on, but there's <span className="red">always</span> <span className="green">more to come</span><span className="yellow">!</span>
         </p>
+        <div className="py-2 gap-2 flex flex-row flex-wrap justify-center items-center">
+          <a href={"https://github.com/WilliamCWhite"}>
+            <div className="yellow-box px-2 py-1 gap-2 rounded-md flex w-fit items-center">
+              <img className="w-4 h-4" src={githubIcon} />
+              <p className="">GitHub</p>
+            </div>
+          </a>
+          <a href={"https://www.linkedin.com/in/williamcwhite323"}>
+            <div className="blue-box px-2 py-1 gap-2 rounded-md flex w-fit items-center">
+              <img className="w-4 h-4" src={linkedinIcon} />
+              <p className="">LinkedIn</p>
+            </div>
+          </a>
+
+
+        </div>
       </div>
       <div className="my-2 w-full h-px bg-ncomment md:w-px md:h-auto md:my-0 md:mx-4"></div>
       <div className="md:w-1 md:grow">
@@ -24,6 +43,7 @@ function AboutSlide() {
           <Skill skillKey="python"/>
           <Skill skillKey="docker"/>
           <Skill skillKey="tailwind"/>
+          <Skill skillKey="reactNative"/>
           <Skill skillKey="nodejs"/>
           <Skill skillKey="java"/>
           <Skill skillKey="nginx"/>
